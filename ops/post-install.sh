3cc913d2451424de79029ce0ae4f2df5442d1df1
Pedro Gomes <pedrogomes94@gmail.com>
2020-01-24 16:08:01 +0100

    update postinstall

--- ops/post-install.sh
+++ ops/post-install.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 echo "PostInstall script:"
 
-echo "0. React Native nodeify..."
+echo "1. React Native nodeify..."
 node_modules/.bin/rn-nodeify --install --hack
-
-# The build output from aes-js breaks the metro bundler. Until we safely upgrade
-# to a new version of aes-js, we patch it by removing the erroneous line.
-echo "1. Fix aes-js build ouput..."
-AES_OUTPUT_FILE="node_modules/aes-js/index.js";
-sed -i'' -e 's/var previous_mymodule = root.mymodule;//g' $AES_OUTPUT_FILE;
 
 echo "2. Fix xmlhttprequest"
 TARGET="node_modules/xmlhttprequest/lib/XMLHttpRequest.js"
 sed -i'' -e 's/var spawn /\/\/var spawn/' $TARGET;
 
-echo "3. Connext v2 fixes"
+# echo "3. Connext v2 fixes"
 TARGET="node_modules/websocket-nats/lib/nats.js"
 # set default nats port
 sed -i'' -e 's/this.url = url;/this.url = url;this.url.port = 4222;/' $TARGET;
